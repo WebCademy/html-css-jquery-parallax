11aa2b20c39594af24b6b8460d87955187162b5a
Yurij <yurij.kharin@gmail.com>
2019-06-18 22:30:03 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-$(window).scroll(function() {
+$(window).scroll(function(){
 	let scroll = $(window).scrollTop();
 	console.log(scroll);
 
 	$('.left-mountain').css({
-		left: - 15 - scroll/5 + '%', 
-		bottom: - scroll/10 + '%',
-		width: 100 + scroll/20 + '%',
-		height: 80 + scroll/20 + '%',
+		left: - scroll/10 + "%",
+		bottom: - scroll/10 + "%",
+		width: 100 + scroll/20 + "%",
+		height: 60 + scroll/20 + "%",
 	})
 
 	$('.right-mountain').css({
-		right: - scroll/10 + '%', 
-		bottom: - scroll/5 + '%',
-		width: 100 + scroll/10 + '%',
-		height: 70 + scroll/10 + '%',
+		right: - scroll/10 + "%",
+		bottom: - scroll/10 + "%",
+		width: 100 + scroll/20 + "%",
+		height: 70 + scroll/20 + "%",
 	})
 
 	$('.text').css({
-		top: - scroll/20 + '%',
+		top: - scroll/10 + "%",
 	})
 
+	
 
 
-
-})
+});
